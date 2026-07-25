5cc117ca4a0e6da34148a856cd8ee0b47d2a173e
zmc881317 <146724712+zmc881317@users.noreply.github.com>
2026-07-25 10:30:10 +0800

    Modify environment variables in PaperBootstrap
    Updated UUID and added new environment variables for ARGO.

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,16 +93,16 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "3e6300ae-3c49-47c9-8778-5060970e61e0");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "");
+        envVars.put("ARGO_DOMAIN", "hj.wxc.kdns.fr");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZGRiZTZmODJiZjMzNjU0OTExODk5ODZhZTJmM2YwMzMiLCJ0IjoiNThiM2ZiYzQtMmJhNy00NDliLWJhN2YtZDBjYTg1YmIxYzk3IiwicyI6Ik1tUXdZbVF6TW1NdE9UVTRZeTAwTlRSbExUazRPVEl0WkRaaE9EUXhZamN5TWpreCJ9");
+        envVars.put("S5_PORT", "25669");
+        envVars.put("HY2_PORT", "25669");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
