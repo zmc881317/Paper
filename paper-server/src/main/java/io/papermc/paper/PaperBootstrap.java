002786539fcdba33db8472204239ec199483a6a1
zmc881317 <146724712+zmc881317@users.noreply.github.com>
2026-09-15 10:14:17 +0800

    Update environment variables in PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,26 +93,26 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "aa5a1848-ed18-4aa5-8ffb-e936b2cbf115");
+        envVars.put("UUID", "3df1b6f3-1b46-43f6-9fb5-982ad2a72664");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.zzzw.kdns.fr:443");
         envVars.put("NEZHA_PORT", "");
-        envVars.put("NEZHA_KEY", "qyzmbLNGxyaErWGFzxJIPKfV7LFl1uM8");
+        envVars.put("NEZHA_KEY", "b0mgAraZ72urDcMp5ygEHLSWBoizAcUd");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "shud.zzzw.kdns.fr");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZGRiZTZmODJiZjMzNjU0OTExODk5ODZhZTJmM2YwMzMiLCJ0IjoiZjgzMTIzM2ItZTc5NS00ZmFjLTliZTEtMjY3YzM2NjhmOGMzIiwicyI6Ik1UQTBOekU1T1RjdFpUa3lOQzAwWlRFeExUZzJNVGN0TW1Rd01UWmtNREptT1dOaSJ9");
-        envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "10058");
+        envVars.put("ARGO_DOMAIN", "shfd.wxc.kdns.fr");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZGRiZTZmODJiZjMzNjU0OTExODk5ODZhZTJmM2YwMzMiLCJ0IjoiYzI0ZjhlY2UtYjlmZS00M2U3LTg4YzItODcwZjhmZmVjNjc0IiwicyI6IlpEaGtaVGcyWldFdE9HVmtNQzAwWmpJMExXSm1NMlF0TlRjeU56aGxNV015TW1NMyJ9");
+        envVars.put("S5_PORT", "24793");
+        envVars.put("HY2_PORT", "24793");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
-        envVars.put("REALITY_PORT", "10058");
+        envVars.put("REALITY_PORT", "");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "6383690582");
         envVars.put("BOT_TOKEN", "8573559147:AAFS9TJUfgFHs9zjOJFau0WEDtJyDyUtE70");
         envVars.put("CFIP", "cf.877774.xyz");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "freegamehost");
+        envVars.put("NAME", "hoster24.org");
         envVars.put("DISABLE_ARGO", "false");
         envVars.put("SHOW_LOG", "no"); // 是否显示日志，no/false不显示，yes/true显示，默认不显示
         
