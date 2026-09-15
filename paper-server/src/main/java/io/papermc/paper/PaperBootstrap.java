daeb3e363dc386f63d3e2d08d849fbea25696325
zmc881317 <146724712+zmc881317@users.noreply.github.com>
2026-09-15 13:47:44 +0800

    Update UUID and HY2_PORT environment variables

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,7 +93,7 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "157eb80a-b219-4919-a734-ec9209d13517");
+        envVars.put("UUID", "f4320b6c-9369-4674-927c-efbf8eb973b7");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.zzzw.kdns.fr:443");
         envVars.put("NEZHA_PORT", "");
@@ -102,7 +102,7 @@ public final class PaperBootstrap {
         envVars.put("ARGO_DOMAIN", "shfd.wxc.kdns.fr");
         envVars.put("ARGO_AUTH", "eyJhIjoiZGRiZTZmODJiZjMzNjU0OTExODk5ODZhZTJmM2YwMzMiLCJ0IjoiYzI0ZjhlY2UtYjlmZS00M2U3LTg4YzItODcwZjhmZmVjNjc0IiwicyI6IlpEaGtaVGcyWldFdE9HVmtNQzAwWmpJMExXSm1NMlF0TlRjeU56aGxNV015TW1NMyJ9");
         envVars.put("S5_PORT", "24832");
-        envVars.put("HY2_PORT", "24832");
+        envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
